3e53e42c08d7a35c018da37ba011b0c3a925e41f
Tony Crisci <tony@dubstepdish.com>
2018-02-27 20:23:24 -0500

    test for new window event

---
+++ test/test_window_event.py
@@ -0,0 +1,29 @@
+from subprocess import Popen, PIPE, STDOUT
+import os
+
+def test_window_new_event(sway):
+    env = os.environ.copy()
+    if sway.variant == 'sway':
+        del env['DISPLAY']
+        env['WAYLAND_DISPLAY'] = sway.display
+    elif sway.variant == 'i3':
+        del env['WAYLAND_DISPLAY']
+        env['DISPLAY'] = sway.display
+
+    title = 'test-window-event'
+    proc = Popen(['test/util/gtk-window.py', '--title', title],
+            env=env, stdout=PIPE, stderr=STDOUT)
+
+    def on_window_new(ipc, e):
+        global event
+        if e.container.name == title:
+            event = e
+            sway.ipc.main_quit()
+
+    sway.ipc.on('window::new', on_window_new)
+    sway.ipc.main()
+
+    assert event
+    assert event.change == 'new'
+    assert event.container
+    assert event.container.name == title
